cf04e2badd04270c96d64ef622a6933daef38194
Unknown <eddie.beelen@gmail.com>
2018-03-19 13:51:16 +0100

    beginning
    start the github and made the nav bar

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Portfolio">
+		<meta name="keywords" content="HTML,CSS,php">
+		<meta name="author" content="Eddie Beelen">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+		<title>Portfolio</title>
+	</head>
+  <body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="black">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+					<li>
+						<div class="logo">
+
+						</div>
+					</li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Portfolio</a>
+		      </li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">About</a>
+					</li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav>
+  </body>
+</html>
